f91d264fcc0819c4f539002e61245d09747249e7
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-06-01 22:43:44 -0600

    Create for8.java

---
+++ for8.java
@@ -0,0 +1,12 @@
+public class for8 {
+    public static void main(String[] args) {
+        int a;
+        for(a = 1; a >=1; a--){
+            System.out.println("A   A+2   A+4   A+6");
+            System.out.print("3    5     7     9\n");
+            System.out.print("6    8     10    12\n");
+            System.out.print("9    11    13    15\n");
+            System.out.print("12   14    16    18\n");
+        }
+    }
+}
